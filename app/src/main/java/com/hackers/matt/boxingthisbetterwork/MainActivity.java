dc3862389c06ae7b4bae4b6ee3a68f4fcb8d9410
Ali Zaidi <alizaidi54321@gmail.com>
2017-10-07 17:58:05 -0500

    Sound Functionality

--- app/src/main/java/com/hackers/matt/boxingthisbetterwork/MainActivity.java
+++ app/src/main/java/com/hackers/matt/boxingthisbetterwork/MainActivity.java
@@ -7,13 +7,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.os.CountDownTimer;
+import android.media.AudioTrack;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import org.w3c.dom.Text;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    public int counter,pausedCounter = 0;
     public boolean paused = false;
-    public boolean canceled = false;
-    long milliLeft,min,sec;
+    long milliLeft = 300000;
+    int secondsPassed = 0 ;
+    long min,sec;
     Button button,button2,button3;
     TextView textView;
 
@@ -25,18 +29,26 @@ public class MainActivity extends AppCompatActivity {
         button2 = (Button) findViewById(R.id.Pause);
         button3 = (Button) findViewById(R.id.Reset);
         textView = (TextView) findViewById(R.id.countdownClock);
-        final CountDownTimer ctd = new CountDownTimer(300000, 1000) {
+        final CountDownTimer ctd = new CountDownTimer(65000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                milliLeft = millisUntilFinished;
-                min = (milliLeft/(1000*60));
-                sec = (milliLeft/1000 - min * 60);
-                textView.setText(Long.toString(min)+ ":" + Long.toString(sec));
+                milliLeft = millisUntilFinished - (secondsPassed * 1000);
+                min = (millisUntilFinished/(1000*60));
+                sec = (millisUntilFinished/1000 - min * 60);
+                if(sec < 10)
+                    textView.setText(Long.toString(min) + ":0"+  Long.toString(sec));
+                else
+                    textView.setText(Long.toString(min)+ ":" + Long.toString(sec));
+                secondsPassed++;
+                playCombo();
+                if(millisUntilFinished == 30 * 1000)
+                    playWarning();
             }
 
             @Override
             public void onFinish() {
-                textView.setText("FINISH!!!!");
+                textView.setText("0:00");
+                playRest();
             }
 
         };
@@ -47,25 +59,86 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-       /** Trying to get pause button to work...
-        *
-        *
-        * button2.setOnClickListener(new View.OnClickListener() {
+        button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
                 ctd.cancel();
-                paused = true;
             }
         });
-*/
+
         button3.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
                 ctd.cancel();
-                milliLeft = 300000;
+                secondsPassed = 0;
             }
         });
+    }
+
+    public void playRest(){
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.timer_finished);
+        mp.setVolume(0.05f, 0.05f);
+        mp.start();
+        System.out.print("rest");
+    }
+
+    public void playCombo() {
+        MediaPlayer mp = null;
+        Random rand = new Random();
+        int numMoves = rand.nextInt(6);
+        for (int i = 0; i < numMoves; i++) {
+            int move = rand.nextInt(12);
+            switch (move) {
+                case 0:
+                    mp = MediaPlayer.create(this, R.raw.bob);
+                    mp.start();
+                    mp = null;
+                    break;
+                case 1:
+                    mp = MediaPlayer.create(this, R.raw.slipl);
+                    mp.start();
+                    break;
+                case 2:
+                    mp = MediaPlayer.create(this, R.raw.slipr);
+                    mp.start();
+                    break;
+                case 3:
+                    mp = MediaPlayer.create(this, R.raw.one);
+                    mp.start();
+                    break;
+                case 4:
+                    mp = MediaPlayer.create(this, R.raw.two);
+                    mp.start();
+                    break;
+                case 5:
+                    mp = MediaPlayer.create(this, R.raw.three);
+                    mp.start();
+                    break;
+                case 6:
+                    mp = MediaPlayer.create(this, R.raw.four);
+                    mp.start();
+                    break;
+                case 7:
+                    mp = MediaPlayer.create(this, R.raw.five);
+                    mp.start();
+                    break;
+                case 8:
+                    mp = MediaPlayer.create(this, R.raw.six);
+                    mp.start();
+                    break;
+                default:
+                    break;
+            }
+            mp = null;
+        }
+    }
+
+    public void playWarning() {
+        System.out.println("warning");
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.round_warning);
+        mp.setVolume(0.05f, 0.05f);
+        mp.start();
     }
 }
 
